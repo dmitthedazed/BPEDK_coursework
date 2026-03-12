55e0123d9312407220c8b9eba5364220d68e7731
dmitthedazed <dmitriyuzat@gmail.com>
2026-03-12 23:50:46 +0100

    Restructure survey pages and add entrypoint navigation

---
+++ survey/educators/links.js
@@ -0,0 +1,7 @@
+window.SURVEY_LINKS = [
+  "https://github.com",
+  "https://developer.mozilla.org",
+  "https://www.wikipedia.org",
+  "https://news.ycombinator.com",
+  "https://www.nasa.gov"
+];
---
+++ survey/parents/links.js
@@ -0,0 +1,7 @@
+window.SURVEY_LINKS = [
+  "https://www.bbc.com",
+  "https://www.nationalgeographic.com",
+  "https://www.khanacademy.org",
+  "https://www.ted.com",
+  "https://www.unicef.org"
+];
